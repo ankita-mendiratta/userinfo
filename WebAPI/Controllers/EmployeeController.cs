f246daa8db5e68287f9af732cf5bfc300f351b3d
Ankita Mendiratta <ankita.iftm@yahoo.com>
2019-09-30 20:03:21 +0530

    First commit

---
+++ WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.Models;
+using System.Web.Http.Cors;
+
+namespace WebAPI.Controllers
+{
+    //[EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class EmployeeController : ApiController
+    {
+        private DBModel db = new DBModel();
+
+        // GET api/Employee
+        public IQueryable<User> GetUsers()
+        {
+            return db.Employees;
+        }
+
+
+        // PUT api/Employee/5
+        public IHttpActionResult PutUser(int id, User user)
+        {
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Employee
+        [ResponseType(typeof(User))]
+        public IHttpActionResult PostUsere(User user)
+        {
+
+            db.Employees.Add(user);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = user.UserID }, user);
+        }
+
+        // DELETE api/Employee/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult DeleteUser(int id)
+        {
+            User user = db.Employees.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.Employees.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserExists(int id)
+        {
+            return db.Employees.Count(e => e.UserID == id) > 0;
+        }
+    }
+}
